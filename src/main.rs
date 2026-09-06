e15ce3f857b557a7dda9f3dc0999a1e1adaa1cf5
Muhammad Faizud Daroin <m@faizud.net>
2026-09-07 06:45:08 +0700

    fix: aim system

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::f32::consts::PI;
+use std::f32::consts::{PI, TAU};
 
-use bevy::{color::palettes::css::PURPLE, prelude::*};
+use bevy::prelude::*;
 
 fn main() {
     App::new()
@@ -10,17 +10,16 @@ fn main() {
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d);
     commands.spawn(PlayerBundle::new(asset_server.load("stone01.png")));
-    commands.spawn(AimBundle::new(
-        meshes.add(Rectangle::default()),
-        materials.add(Color::from(PURPLE)),
+    commands.spawn((
+        AimBundle::new(),
+        children![
+            SpriteBundle::new(asset_server.load("placeholder.png"))
+                .with_scale(Vec3::new(1.0, 0.5, 1.0))
+                .with_pos(Vec3::new(32.0, 0.0, 0.0))
+        ],
     ));
 }
 
@@ -29,9 +28,10 @@ fn aim_rotate(time: Res<Time>, mut aims: Query<(&mut Transform, &mut Speed, &mut
         let delta = speed.0 * time.delta_secs();
         aim.0 += delta;
         if aim.0 > PI || aim.0 < 0.0 {
+            aim.0 = aim.0.clamp(0.0, PI);
             speed.0 = -speed.0;
         }
-        transform.rotate_z(delta);
+        transform.rotation = Quat::from_euler(EulerRot::YXZ, 0.0, 0.0, aim.0);
     }
 }
 
@@ -46,11 +46,11 @@ struct PlayerBundle {
 }
 
 impl PlayerBundle {
-    fn new(asset: Handle<Image>) -> Self {
+    fn new(image: Handle<Image>) -> Self {
         return Self {
             player: Player,
-            sprite: Sprite::from_image(asset),
-            transform: Transform::IDENTITY,
+            sprite: Sprite::from_image(image),
+            transform: Transform::default(),
         };
     }
 }
@@ -65,19 +65,44 @@ struct Speed(f32);
 struct AimBundle {
     aim: Aim,
     speed: Speed,
-    mesh: Mesh2d,
-    material: MeshMaterial2d<ColorMaterial>,
     transform: Transform,
 }
 
 impl AimBundle {
-    fn new(mesh: Handle<Mesh>, color: Handle<ColorMaterial>) -> Self {
+    fn new() -> Self {
         return Self {
             aim: Aim(0.0),
-            speed: Speed(PI),
-            mesh: Mesh2d(mesh),
-            material: MeshMaterial2d(color),
-            transform: Transform::default().with_scale(Vec3::splat(128.)),
+            speed: Speed(TAU),
+            transform: Transform::default(),
         };
+    }
+}
+
+#[derive(Bundle)]
+struct SpriteBundle {
+    sprite: Sprite,
+    transform: Transform,
+}
+
+impl SpriteBundle {
+    fn new(image: Handle<Image>) -> Self {
+        return Self {
+            sprite: Sprite::from_image(image),
+            transform: Transform::default(),
+        };
+    }
+
+    #[inline]
+    #[must_use]
+    fn with_pos(mut self, pos: Vec3) -> Self {
+        self.transform.translation = pos;
+        return self;
+    }
+
+    #[inline]
+    #[must_use]
+    fn with_scale(mut self, scale: Vec3) -> Self {
+        self.transform.scale = scale;
+        return self;
     }
 }
